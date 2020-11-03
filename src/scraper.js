3285091a5280dbe4b706f3fe8a77200f8c776ebf
Gaurav Pandey <gauravguitarrocks@gmail.com>
2020-11-04 01:26:26 +0530

    url: Update url schema for puppeteer.

--- src/scraper.js
+++ src/scraper.js
@@ -2,6 +2,7 @@ const puppeteer = require("puppeteer");
 
 module.exports.query_result = function(queryoptions){
     let url = url_generate(queryoptions);
+
     return new Promise((resolve,reject) => {
         resolve(browseractions(url))
     });
@@ -13,26 +14,70 @@ async function browseractions(url){
     await page.goto(url);
     console.log(url)
     const elements =  await page.$$("#internship_list_container_1 .individual_internship");
-    var internships = [];
-    elements.forEach(async (node) => {
-        let title = (await (await node.$eval(".heading_4_5 a",title => {return title.innerText})));
-        let link = "https://internshala.com/" + (await (await node.$eval(".heading_4_5 a",link => {return link.getAttribute("href")})));
+
+    return job_array_generate(elements);
+}
+
+async function job_array_generate(dom_elements){
+    const internships =  dom_elements.map(async (node) => {
+        let title = (await node.$eval(".heading_4_5 a",title => {return title.innerText}));
+        let link = "https://internshala.com" + (await node.$eval(".heading_4_5 a",link => {return link.getAttribute("href")}));
+        let location = (await node.$eval(".location_link",location => {return location.innerText}));
+        let start_date = (await node.$eval(".start_immediately_desktop",start_date => {return start_date.innerText}));
+        let apply_by = (await node.$eval(".apply_by .item_body",apply_by => {return apply_by.innerText}));
+        let stipend = "₹" + (await node.$eval(".stipend",stipend => {return stipend.innerText}));
+
         let internship_individual = {
             "title": title,
             "link": link,
+            "location": location,
+            "startdate": start_date,
+            "stipend": stipend,
+            "applyby": apply_by,
         };
-        internships.push(internship_individual);
-        console.log(internship_individual);
+
+        return internship_individual;
     });
-    return internships;
-}
+
+    return await Promise.all(internships);
+};
 
 function url_generate(queryoptions){
+    var pre_url = "https://internshala.com/internships/";
+    let url = "";
     var jobs = "";
+
     queryoptions.jobs.forEach((val) => {
         jobs += job_match(val) + ",";
     });
-    let url = "https://internshala.com/internships/"+jobs+"-internship";
+
+    if(queryoptions.work_from_home){
+        pre_url += "work-from-home-";
+        url = pre_url+jobs+"-jobs";
+    }else{
+        url = pre_url+jobs+"-internship";
+    }
+
+    if(queryoptions.locations){
+        url += "-in-";
+
+        queryoptions.locations.forEach((val) => {
+            url += val.toLowerCase() + ",";
+        });
+
+        url += "/";
+    }else{
+        url += "/";
+    }
+
+    url += "duration-" + queryoptions.max_duration.toString() + "/";
+
+    url += "start_date-" + queryoptions.start_date.split("-").join("") + "/";
+
+    if(queryoptions.job_offer){
+        url += "ppo-true/"
+    }
+
     return url;
 }
 
