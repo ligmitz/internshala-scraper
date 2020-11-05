93387b9802c47c81c05748b447937003dd48548b
Gaurav Pandey <gauravguitarrocks@gmail.com>
2020-11-06 00:31:11 +0530

    prettier: Install prettier.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const scraper = require("./src/scraper.js");
 
-module.exports.query = function(queryoptions){
-    return scraper.query_result(queryoptions);
-}
+module.exports.query = function (queryoptions) {
+  return scraper.query_result(queryoptions);
+};
